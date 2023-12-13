311e5fe7f53d110734a7a687573d11caa862a650
H1te4 <129965141+kirill2342312@users.noreply.github.com>
2023-12-13 12:18:23 +0300

    Add files via upload

---
+++ Алгоритмизация/5-6 LR/2 задание 5-6.pas
@@ -0,0 +1,19 @@
+﻿program zad2;
+var
+A: array [1..20] of integer;
+i,b,c:integer;
+begin
+  c:=1;
+  for i:=1 to 20 do
+    begin
+    A[i]:=random(-22,93);
+    if (i mod 2 = 1) and (A[i] mod 2 = 0) then
+      b:=b+1;
+    begin
+    if (i mod 2 = 1) then
+      c:=c*A[i]
+    end;
+    write(c:20)
+  end;
+   write(b)
+end.
---
+++ Алгоритмизация/5-6 LR/4 задание 5-6.pas
@@ -0,0 +1,38 @@
+﻿program ArrayOperations;
+var
+  arr: array[1..20] of integer;
+  i, maxElement, minPositiveElement, maxElementIndex, minPositiveElementIndex, lastMultipleOf5Index: integer;
+begin
+  for i := 1 to 20 do
+    arr[i] := Random(118) - 52; // 118 = 65 - (-52) + 1
+  maxElement := arr[1];
+  minPositiveElement := MaxInt;
+  maxElementIndex := 1;
+  minPositiveElementIndex := 0;
+  lastMultipleOf5Index := 0;
+  for i := 1 to 20 do
+  begin
+    if arr[i] > maxElement then
+    begin
+      maxElement := arr[i];
+      maxElementIndex := i;
+    end;
+    if (arr[i] > 0) and (arr[i] < minPositiveElement) then
+    begin
+      minPositiveElement := arr[i];
+      minPositiveElementIndex := i;
+    end; 
+    if arr[i] mod 5 = 0 then
+      lastMultipleOf5Index := i;
+  end;
+  writeln('Массив:');
+  for i := 1 to 20 do
+    write(arr[i], ' ');
+
+  writeln;
+  writeln('Наибольший элемент массива: ', maxElement);
+  writeln('Номер наибольшего элемента: ', maxElementIndex);
+  writeln('Наименьший положительный элемент массива: ', minPositiveElement);
+  writeln('Номер наименьшего положительного элемента: ', minPositiveElementIndex);
+  writeln('Номер последнего элемента, кратного 5: ', lastMultipleOf5Index);
+end.
